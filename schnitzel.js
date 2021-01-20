87019c8121d1d4c81f24c2f0b6024c775dafee64
Noah Zarro <noahzarro@gmail.com>
2021-01-20 12:15:05 +0100

    initial commit

---
+++ schnitzel.js
@@ -0,0 +1,10 @@
+function test() {
+    let keyword = document.getElementById("keyword").value
+    if (keyword == "Lano") {
+        alert("Artscholocco, das isch jo es grässlichs Wort");
+    } else if (keyword == "Arschwasser") {
+        alert("Richtig! S Passwort isch: 'Hello I'm Vegan'");
+    } else {
+        alert("S Wort isch falsch gsi...");
+    }
+}
